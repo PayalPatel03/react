3a7e3621a7cd5e51ee61b22ff26141cd5eb52c06
PayalPatel03 <p6579113@gmail.com>
2025-05-30 16:32:17 +0530

    donee

--- daily/json-server/src/App.jsx
+++ daily/json-server/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleFetch = () => {
+    fetch('http://localhost:3000/posts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      console.log("data fetched");
+      return res.json();
+    }).then((data) => {
+      console.log(data);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
+  const handleAddData = () => {
+    let obj = {
+      name: 'acb',
+      age: 22
+    };
+
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+    }).then((res) => {
+      console.log("data added");
+      return res.json();
+    }).then((data) => {
+      console.log(data);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className='btn btn-primary' onClick={handleFetch}>Fetch Data</button>
+      <button className='btn btn-primary' onClick={handleAddData}>Add Data</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
