f9908d3f33634cfe41c0ae9ab8f925661ad68d1f
PayalPatel03 <p6579113@gmail.com>
2025-07-08 13:08:41 +0530

    done

---
+++ daily/crud-firestore/src/firebase/config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqo4iV9JNlkynjKtvpQ7wEdi9aO9MfL3M",
+  authDomain: "firestore-crud-1fb36.firebaseapp.com",
+  projectId: "firestore-crud-1fb36",
+  storageBucket: "firestore-crud-1fb36.firebasestorage.app",
+  messagingSenderId: "40135083133",
+  appId: "1:40135083133:web:b5c0dbeda982a789259d14"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
